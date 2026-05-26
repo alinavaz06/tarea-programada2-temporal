028b92519a7a9ba715c13bc687162e5c3c09c82e
Alina Vazquez <alinakid06@gmail.com>
2026-05-25 20:42:50 -0600

    correccion

--- TareaP2/GeneradorDeGrupos.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GENERADOR_GRUPOS
-#define _GENERADOR_GRUPOS
-
-// nicole
-class GeneradoDeGrupos{ //plantillas
-
-};
-#endif
--- TareaP2/LectorDePerfiles.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LECTOR_PERFILES
-#define _LECTOR_PERFILES
-
-// paz
-class LectorDePerfiles{
-
-};
-
-#endif
